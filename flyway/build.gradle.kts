1547e59e5d81370dbc73b2fbac36c3182f8384b6
morisoto <k.morisoto@quo-digital.jp>
2024-08-31 21:24:57 +0900

    initial commit

---
+++ flyway/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":db"))
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ flyway/db/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("jvm")
+    id("org.jooq.jooq-codegen-gradle") version "3.19.11"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api("org.jooq:jooq:3.19.11")
+    implementation("org.jooq:jooq-meta:3.19.11")
+    implementation("org.jooq:jooq-codegen:3.19.11")
+    implementation("org.jooq:jooq-postgres-extensions:3.19.11")
+
+    runtimeOnly("org.postgresql:postgresql:42.7.4")
+    jooqCodegen("org.postgresql:postgresql:42.7.4")
+}
+
+jooq {
+    configuration {
+
+        jdbc {
+            driver = "org.postgresql.Driver"
+            url = "jdbc:postgresql://localhost:5432/postgres"
+            user = "postgres"
+            password = "password"
+        }
+        generator {
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                inputSchema = "public"
+                includes = ".*"
+            }
+
+            target {
+                packageName = "org.example.db"
+            }
+        }
+    }
+}
+
+sourceSets.main {
+    java.srcDirs("build/generated-sources/jooq")
+}
+
+// https://www.jooq.org/doc/latest/manual/code-generation/codegen-gradle/codegen-gradle-compiler-dependency/
+tasks.named("compileKotlin") {
+    dependsOn(tasks.named("jooqCodegen"))
+}
