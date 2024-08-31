3499f28fe553c3f7ed692dc7f7ce2bef930d4f70
morisoto <k.morisoto@quo-digital.jp>
2024-08-31 14:18:24 +0900

    add gradle

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    id("org.jooq.jooq-codegen-gradle") version "3.19.11"
 }
 
 group = "org.example"
@@ -10,6 +11,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jooq:jooq")
+    implementation("org.jooq:jooq-meta")
+    implementation("org.jooq:jooq-codegen")
+    implementation("org.jooq:jooq-postgres-extensions")
+
+    runtimeOnly("org.postgresql:postgresql:42.7.4")
+    jooqCodegen("org.postgresql:postgresql:42.7.4")
+
     testImplementation(kotlin("test"))
 }
 
@@ -18,4 +27,30 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+
+jooq {
+    configuration {
+
+        // Configure the database connection here
+        jdbc {
+            driver = "org.postgresql.Driver"
+            url = "jdbc:postgresql://localhost:5432/postgres"
+            user = "postgres"
+            password = "password"
+        }
+        generator {
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                inputSchema = "public"
+                includes = ".*"
+            }
+
+            target {
+                packageName = "org.example.db"
+                directory = "build/generated-src/jooq/main"
+            }
+        }
+    }
 }
