0c780b9d833336eaf73f40ce5ea3127017a7ea44
morisoto <k.morisoto@quo-digital.jp>
2024-09-07 16:34:11 +0900

    add sample

---
+++ spring-boot-flyway/src/main/resources/db/migration/V2__create_categories.sql
@@ -0,0 +1,5 @@
+CREATE TABLE categories
+(
+    id   serial PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
---
+++ spring-boot-flyway/src/main/resources/db/migration/V3__Insert_initial_categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (name)
+VALUES ('Electronics')
+     , ('Books')
+     , ('Clothing');
