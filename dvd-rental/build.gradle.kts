a1de3848c5d60172c658112fd9fee3ad6e1b18ab
morisoto <k.morisoto@quo-digital.jp>
2024-09-24 21:30:54 +0900

    add sample

--- dvd-rental/build.gradle.kts
+++ dvd-rental/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("org.jooq.jooq-codegen-gradle") version "3.19.11"
 }
 
 group = "org.example"
@@ -10,6 +11,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jooq:jooq:3.19.11")
+    implementation("org.jooq:jooq-meta:3.19.11")
+    implementation("org.jooq:jooq-codegen:3.19.11")
+    implementation("org.jooq:jooq-postgres-extensions:3.19.11")
+
+    runtimeOnly("org.postgresql:postgresql:42.7.4")
+    jooqCodegen("org.postgresql:postgresql:42.7.4")
+
     testImplementation(kotlin("test"))
 }
 
@@ -18,4 +27,45 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+
+jooq {
+    configuration {
+
+        jdbc {
+            driver = "org.postgresql.Driver"
+            url = "jdbc:postgresql://localhost:5432/dvdrental"
+            user = "postgres"
+            password = "password"
+        }
+        generator {
+            generate {
+                name = "org.jooq.codegen.KotlinGenerator"
+                isPojos = true
+                isInterfaces = true
+                isKotlinNotNullPojoAttributes = true
+                isKotlinNotNullRecordAttributes = true
+                isKotlinNotNullInterfaceAttributes = true
+            }
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                inputSchema = "public"
+                includes = ".*"
+            }
+
+            target {
+                packageName = "org.example.db"
+            }
+        }
+    }
+}
+
+sourceSets.main {
+    java.srcDirs("build/generated-sources/jooq")
+}
+
+// https://www.jooq.org/doc/latest/manual/code-generation/codegen-gradle/codegen-gradle-compiler-dependency/
+tasks.named("compileKotlin") {
+    dependsOn(tasks.named("jooqCodegen"))
 }
